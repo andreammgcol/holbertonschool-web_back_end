099cfa3e0e18265dea4d70cff9bbe90182b8d039
andreammgcol <amendez72@misena.edu.co>
2021-03-15 18:06:36 -0500

    fixing type float

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,7 +3,7 @@
 """ Basic annotations - to string """
 
 
-def to_str(n: str) -> str:
+def to_str(n: float) -> str:
     """ function to_str that takes a float n as argument
     and returns the string representation of the float"""
     return str(n)
