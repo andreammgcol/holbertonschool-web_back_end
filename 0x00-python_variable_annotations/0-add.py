318e3ed655ede1891b3fd4847545bd533655bdcd
andreammgcol <amendez72@misena.edu.co>
2021-03-15 17:41:21 -0500

    fixing style

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,5 +4,6 @@
 
 
 def add(a: float, b: float) -> float:
-	""" type-annotated function add """
-	return a + b
+	""" function add that takes a float a and a float b as
+		arguments and returns their sum as a float """
+	return (a + b)
