5c974b7bbda1be6fd4d21d4cdf989718a4b4ac21
andreammgcol <amendez72@misena.edu.co>
2021-03-22 13:21:15 -0500

    basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -11,12 +11,8 @@ class BasicCache(BaseCaching):
         the item value for the key"""
         if key or item:
             self.cache_data[key] = item
-        else:
-            pass
 
     def get(self, key):
         """ method return the value linked to key"""
         if key:
             return self.cache_data.get(key)
-        else:
-            pass
