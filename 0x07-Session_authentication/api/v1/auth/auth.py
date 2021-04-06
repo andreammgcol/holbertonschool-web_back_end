c631925dcebcc47094f2cb18c256a1caad28f61f
andreammgcol <amendez72@misena.edu.co>
2021-04-06 18:00:51 -0500

    Session cookie

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -31,3 +32,9 @@ class Auth:
         """ Method that returns None
         """
         return None
+
+    def session_cookie(self, request=None):
+        """ Method that returns a cookie value from a request
+        """
+        if request:
+            return request.cookies.get(getenv('SESSION_NAME'))
