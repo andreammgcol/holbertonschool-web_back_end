647acb795dd9c3596a1dcca69fba70d23020abc7
andreammgcol <amendez72@misena.edu.co>
2021-04-20 18:17:51 -0500

    update exception

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Test module """
 
 import unittest
 from utils import access_nested_map
@@ -6,11 +7,22 @@ from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-
+    """TestAccessNestedMap class
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """The method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that a KeyError is raised for the following inputs"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
