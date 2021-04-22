c6fbbcd5791198a5b6d16d3be0ec3ba25c5e9130
andreammgcol <amendez72@misena.edu.co>
2021-04-22 18:51:04 -0500

    requirements

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -6,17 +6,18 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
 babel = Babel(app)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
 
 
 class Config:
     """Config class
     """
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
