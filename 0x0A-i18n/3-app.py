561db085f2746d96eaa21f1393e543f554762bdf
andreammgcol <amendez72@misena.edu.co>
2021-04-23 19:34:34 -0500

    fix order

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -20,16 +20,16 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale():
-    """ Get locale from request """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ GET method """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request """
+    return request.accept_languages.best_match(Config['LANGUAGES'])
 
 
 if __name__ == "__main__":
