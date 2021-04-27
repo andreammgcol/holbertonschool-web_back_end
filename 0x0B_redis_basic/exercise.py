783be5abe8b639692cdc50a43f5d925110d258cf
andreammgcol <amendez72@misena.edu.co>
2021-04-27 14:03:11 -0500

    documentation

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -10,10 +10,12 @@ class Cache:
     """ Cache class
     """
     def __init__(self):
+        """ initialize """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ method that takes a data argument and returns a string """
         key = str(uuid.uuid4())
         self._redis.mset({key: data})
         return key
