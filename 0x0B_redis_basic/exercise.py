dd36a95147c8639df60636b2b3b151a6a7ea1a8f
andreammgcol <amendez72@misena.edu.co>
2021-04-27 21:25:25 -0500

    Storing lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,6 +20,19 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ that create input and output list keys, respectively
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.rpush(method.__qualname__ + ':inputs', str(args))
+        output = method(self, *args, **kwds)
+        self._redis.rpush(method.__qualname__ + ':outputs', output)
+        return output
+    return wrapper
+
+
 class Cache:
     """ Cache class
     """
@@ -29,6 +42,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ method that takes a data argument and returns a string """
         key = str(uuid.uuid4())
