ca3fbfbf2bb5cebba6cb5da4fa499d3fd1e4e31b
andreammgcol <amendez72@misena.edu.co>
2021-05-04 21:55:48 -0500

    log stats

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" list of school having a specific topic
+"""
+
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ that returns the list of school having a specific topic """
+    return mongo_collection.find({'topics': topic})
---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" that provides some stats about Nginx logs stored in MongoDB
+"""
+
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    stats = MongoClient('mongodb://localhost:27017').logs.nginx
+    print(f'{stats.count_documents({})} logs')
+    print('Methods:')
+    print(f'\tmethod GET: {stats.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {stats.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {stats.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {stats.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {stats.count_documents({"method": "DELETE"})}')
+    print(f'{c.count_documents({"path": "/status"})} status check')
