354bbe137e2f80e5885e65f96d675965f2a5d4f6
andreammgcol <amendez72@misena.edu.co>
2021-05-15 14:07:52 -0500

    loops

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const new_array = [];
+  for (const idx of array) {
+    new_array.push(`${appendString}${idx}`);
+  }
+  return new_array;
+}
