b9f82e2d1600e88a4847a593967b8c3d04f52758
andreammgcol <amendez72@misena.edu.co>
2021-05-24 16:58:12 -0500

    fixing the closing

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -5,6 +5,6 @@ process.stdin.on('data', (name) => {
   process.stdout.write(`Your name is: ${name}`);
 });
 
-process.on('end', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
